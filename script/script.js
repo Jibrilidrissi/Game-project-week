cc5e890eb497483077b205bea922c6538d915237
Jibrilidrissi <jibrilgamer402@gmail.com>
2025-10-14 15:04:22 +0200

    Het character beweegt en de 2de pagina is toegevoegd

--- script/script.js
+++ script/script.js
@@ -1,31 +1,18 @@
+setInterval(()=>img.style.left=(parseInt(img.style.left)+1)+'px',10);
+
 const stick = document.getElementById("stick");
-console.log(stick)
+console.log(img)
 document.addEventListener('keydown', event => {
-  movestick(event.key)
+  moveimg(event.key)
 });
 
-function movestick(key) 
+function moveimg(key) 
 {
   if ((key == "w" || key == "W")) 
   {
-    stick.style.top = (parseInt(stick.style.top) - 10) + "px";
+    img.style.top = (parseInt(img.style.top) - 10) + "px";
     console.log("UP")
   }
-  else if (key == "s" || key == "S")
-  {
-    stick.style.top = (parseInt(stick.style.top) + 10) + "px";
-    console.log("DOWN")
-  }
-  else if ((key == "a" || key == "A") && parseInt(stick.style.left) > 0 )
-  {
-    stick.style.left = (parseInt(stick.style.left) - 10) + "px";
-    console.log("LEFT")
-  }
-  else if ((key == "d" || key == "D")  && parseInt(stick.style.left) < 550 )
-  {
-    stick.style.left = (parseInt(stick.style.left) + 10) + "px";
-    console.log("RIGHT")
-  }
-
 
 }
+
